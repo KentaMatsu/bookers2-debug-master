bd41949c7ed83016e73dad97d8d3a0b427db2823
KentaMatsu <matsuken102@gmail.com>
2021-07-29 01:49:39 +0000

    finish-debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
+
+  def autheniticate_user
+    if @current_user==nil
+      redirect_to("/login")
+    end
+  end
+
+
   protected
   def after_sign_in_path_for(resource)
     user_path(current_user)
@@ -14,4 +22,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
+
+
+
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :ensure_correct_user, only: [:update]
+  before_action :authenticate_user!
 
   def show
     @book_new = Book.new
@@ -25,6 +25,11 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+    unless @book.user_id == current_user.id
+      redirect_to books_path
+    else
+      render :edit
+    end
   end
 
 
@@ -34,7 +39,7 @@ class BooksController < ApplicationController
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render "edit"
+      render :edit
     end
   end
 
